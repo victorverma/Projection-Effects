d7b12dc8db40397c34e2f36ede4e5c4ae6daabcb
victorverma <vishashv@gmail.com>
2025-08-19 17:15:13 -0400

    Train on all partitions

--- experiment/model_training/train_models.sh
+++ experiment/model_training/train_models.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 n_jobs=${1:--1}
-for i in 1; do
+for i in 1 2 3 4 5; do
     echo "[$(date +'%Y-%m-%d %H:%M:%S')] Training partition $i model..."
     conda run -p ../../env/ --live-stream python train_model.py \
         --partition $i \
