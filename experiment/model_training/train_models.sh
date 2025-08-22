0b48c5f91101fb50adf4cbf173f4e9e9bd2a3216
victorverma <vishashv@gmail.com>
2025-08-21 23:53:17 -0400

    Train models on both kinds of data

--- experiment/model_training/train_models.sh
+++ experiment/model_training/train_models.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+set -e
+
 n_jobs=${1:--1}
 for i in 1 2 3 4 5; do
-    echo "[$(date +'%Y-%m-%d %H:%M:%S')] Training partition $i model..."
-    conda run -p ../../env/ --live-stream python train_model.py \
-        --partition $i \
-        --n_jobs $n_jobs
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] Training partition $i models..."
+    conda run -p ../../env/ --live-stream sh -c "
+        set -e
+        python train_model.py --partition $i --n_jobs $n_jobs
+        python train_model.py --partition $i --use_corrected_data --n_jobs $n_jobs
+    "
 done
