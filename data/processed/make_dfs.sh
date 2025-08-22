2f3ee5a2a09fb62d995f9c0bd11796c3401ec469
victorverma <vishashv@gmail.com>
2025-08-21 23:50:05 -0400

    Move echo outside conda run

--- data/processed/make_dfs.sh
+++ data/processed/make_dfs.sh
@@ -3,9 +3,9 @@
 set -e
 
 for i in 1 2 3 4 5; do
+    echo "Making data frames for partition $i..."
     conda run -p ../../env/ --live-stream sh -c "
         set -e
-        echo 'Partition $i'
         python make_full_df.py --partition $i
         python correct_full_df.py --partition $i
         python summarize_full_df.py --partition $i
