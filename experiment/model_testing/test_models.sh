20c2c54af25fb3dbc60991818e034d4f64cb96f7
victorverma <vishashv@gmail.com>
2025-08-25 14:29:40 -0400

    Change script name

--- experiment/model_testing/test_models.sh
+++ experiment/model_testing/test_models.sh
@@ -2,5 +2,5 @@
 
 set -e
 max_workers=${1:--1}
-conda run -p ../../env/ --live-stream python test_model.py \
+conda run -p ../../env/ --live-stream python test_models.py \
     --max_workers $max_workers
